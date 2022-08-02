07bcbd326bb2ecc3f190bcc94fba7ca4a4e8294d
Алексей Назаров <AlSNazarov@1cbit.ru>
2022-08-02 21:08:11 +0500

    update module, add docs

--- local/modules/anz.appointment/include.php
+++ local/modules/anz.appointment/include.php
@@ -1,3 +1,20 @@
 <?php
 
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\Loader;
+
+$moduleId = 'anz.appointment';
+
+try
+{
+    $arControllers = [
+        '\\ANZ\\Appointment\\Controllers\\OneCController'  => 'lib/Controllers/OneCController.php',
+        '\\ANZ\\Appointment\\Controllers\\MessageController' => 'lib/Controllers/MessageController.php',
+    ];
+    Loader::registerAutoLoadClasses($moduleId, $arControllers);
+}
+catch(Exception $e)
+{
+    Debug::writeToFile(date('d.m.Y H:i:s') . " " . $e->getMessage(), 'Error in include.php');
+}
 ?>
